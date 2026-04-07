34521bf405d7d735344d57d4348f1f9c971b790c
Keith Heffington <dransiktolkien@yahoo.com>
2026-04-07 08:33:12 -0500

    JavaScript
    Work on some JavaScript code

---
+++ Basic_HTML_and_JavaScript_1/JS/Basic_JavaScript_1.js
@@ -0,0 +1,7 @@
+alert("Hello, World!");
+	
+function My_First_Function() {
+	var String1 = "Kiss me, I'm Irish!";
+	var result = String1.fontcolor("green");
+	document.getElementById("Irish").innerHTML = result;
+}
