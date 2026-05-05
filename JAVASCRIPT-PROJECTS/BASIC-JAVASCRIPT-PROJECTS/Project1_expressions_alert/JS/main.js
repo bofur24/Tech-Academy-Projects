c1fbe2352e7b3f268593f40304e96524556e4419
Keith Heffington <dransiktolkien@yahoo.com>
2026-05-05 09:09:33 -0500

    Added comments to my JS code

--- JAVASCRIPT-PROJECTS/BASIC-JAVASCRIPT-PROJECTS/Project1_expressions_alert/JS/main.js
+++ JAVASCRIPT-PROJECTS/BASIC-JAVASCRIPT-PROJECTS/Project1_expressions_alert/JS/main.js
@@ -14,6 +14,7 @@ var D = "Be who you are and say what you feel,"
 + " because those who mind don't mater and those who matter don't mind."
 + " - Dr. Seuss";
 document.write(D);
+//Multiple Variable Assignment
 var Family = "The Arezzinis", Dad = "Jeremiah", Mom = "Hermoine", Daughter = "Penny", Son = "Zorro";
 document.write("</br>" + Family + " are a family of 5. " + Dad + " is the father, " + Mom + " is the mother, " + Daughter + " is the daughter, and " + Son + " is the son.");
 document.write("<br>" + (3 + 3));
