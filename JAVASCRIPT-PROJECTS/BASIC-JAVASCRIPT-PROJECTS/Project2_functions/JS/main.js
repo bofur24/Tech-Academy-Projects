d78c9be51c6f3fc68ffda7ff2e7d10ecce36c7b5
Keith Heffington <dransiktolkien@yahoo.com>
2026-05-05 09:28:45 -0500

    Functions Project

---
+++ JAVASCRIPT-PROJECTS/BASIC-JAVASCRIPT-PROJECTS/Project2_functions/JS/main.js
@@ -0,0 +1,7 @@
+// Functions project
+
+// Function to assign two variables and utilize document.getElementById to display the sum of the two variables in an HTML element with the id "sum"
+function variableAssign() {
+    var str = "This is what the button will produce when clicked.";
+    document.getElementById("variables").innerHTML = str;
+}
