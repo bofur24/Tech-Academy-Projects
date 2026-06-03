c27762165ec753a0cdb43c21a596ff5f3a3b3b26
Keith Heffington <dransiktolkien@yahoo.com>
2026-06-03 06:59:35 -0500

    Completed all challages

--- HMTL-AND-CSS-PROJECTS/HTML-CSS/JS/javascript.js
+++ HMTL-AND-CSS-PROJECTS/HTML-CSS/JS/javascript.js
@@ -20,3 +20,15 @@ function openForm() {
 function closeForm() {
     document.getElementById("myForm").style.display = "none";
 }
+
+// Slideshow
+$("#slideshow > div:gt(0)").hide();
+
+setInterval(function() {
+  $('#slideshow > div:first')
+    .fadeOut(1000)
+    .next()
+    .fadeIn(1000)
+    .end()
+    .appendTo('#slideshow');
+}, 3000);
